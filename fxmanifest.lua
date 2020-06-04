32489a90e9b4b63153a540376f445ea5041817ed
Fezz <43589668+FBFezz@users.noreply.github.com>
2020-06-04 19:55:56 +0100

    Update and rename __resource.lua to fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+-- Resource Metadata
+fx_version 'bodacious'
+games { 'gta5' }
+
+author 'iEns'
+description 'RealisticVehicleFailure'
+version '1.0.0'
+
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
+
+server_scripts {
+    'config.lua',
+    'server.lua'
+}
